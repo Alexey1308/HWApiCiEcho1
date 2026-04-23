b994ce7b41d45bc25aa54e88242ee5f85fb3cd81
Oskero Alexey <oskar1308@yandex.ru>
2026-04-23 22:34:12 +0300

    success test

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("some data"));
+    }
+}
